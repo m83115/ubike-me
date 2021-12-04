bfc76d84495a99d81b916c82c29879185d8f0940
m83115 <m83115@yahoo.com.tw>
2021-12-05 07:24:17 +0800

    下拉選單

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,59 @@
 <template>
   <div id="app">
+    <div class="row no-gutters">
+      <!-- 選擇地區 -->
+      <div class="toolbox col-sm-3 p-2 bg-white">
+        <div class="form-group d-flex">
+          <label for="city" class="col-form-label mr-2 text-right">縣市</label>
+          <div class="flex-fill">
+            <select id="city" class="form-control">
+              <!-- 製作下拉選單 -->
+              <option v-bind:value="city.name" v-bind:key="city.name" v-for="city in cityName" >
+                {{ city.name }}
+              </option>
+            </select>
+          </div>
+        </div>
+        <div class="form-group d-flex">
+          <label for="dist" class="col-form-label mr-2 text-right">地區</label>
+          <div class="flex-fill">
+            <select id="dist" class="form-control">
+              <!-- 製作下拉選單 -->
+              <option :value="" :key="" v-for="dist in cityName.find((city) => city.name === select.city).districts">
+                {{  }}
+              </option>
+            </select>
+          </div>
+        </div>
+      </div>
+
+      <!-- 顯示地圖和 UBike 站點 -->
+      <div class="col-sm-9">
+        <div id="map"></div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import cityName from './assets/cityName.json';
+
 export default {
   name: 'App',
+  data: () => ({
+    cityName,
+    select: {
+      city: '臺北市',
+    }
+  }),
 };
 </script>
 
 <style lang="scss">
+  @import 'bootstrap/scss/bootstrap';
+
+#map {
+  height: 100vh;
+  position: relative;
+}
 </style>
